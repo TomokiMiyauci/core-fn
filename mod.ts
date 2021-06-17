b9f1cb40a5d303055361f661f3a567ea96efce7d
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-17 22:37:51 +0900

    :sparkles:  Add RegExp.test function

--- mod.ts
+++ mod.ts
@@ -1,3 +1,4 @@
 // Copyright 2021-present the Core-fn authors. All rights reserved. MIT license.
 export * from "./string/mod.ts";
 export * from "./common/mod.ts";
+export * from "./regexp/mod.ts";
