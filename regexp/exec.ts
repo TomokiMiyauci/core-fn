71168659a86ef0daf95578aa86f0add009f11be4
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-19 22:58:35 +0900

    :sparkles:  Add RegExp#exec function

---
+++ regexp/exec.ts
@@ -0,0 +1,24 @@
+// Copyright 2021-present the Core-fn authors. All rights reserved. MIT license.
+import { curry } from "../deps.ts";
+
+/**
+ * @internal
+ */
+const _exec = (regExp: RegExp, val: string) => regExp.exec(val);
+
+/**
+ * Executes a search on a `string` using a regular expression pattern, and returns an `array` containing the results of that search.
+ * @param regExp - Any `RegExp`
+ * @param val - Any `String`
+ * @returns The result of `regExp.exec(val)`
+ *
+ * @example
+ * ```ts
+ * exec(/^test/, 'test-data') // ['test']
+ * ```
+ *
+ * @beta
+ */
+const exec = curry(_exec);
+
+export { _exec, exec };
---
+++ regexp/exec_test.ts
@@ -0,0 +1,19 @@
+// Copyright 2021-present the Core-fn authors. All rights reserved. MIT license.
+import { assertEquals, equal } from "../dev_deps.ts";
+import { _exec } from "./exec.ts";
+
+Deno.test("exec", () => {
+  const table: [RegExp, string, RegExpExecArray | null][] = [
+    [/test/, "hello", null],
+    [/test/, "data-test", ["test"] as RegExpExecArray],
+    [/^test/, "test-data", ["test"] as RegExpExecArray],
+  ];
+
+  table.forEach(([regExp, val, expected]) => {
+    assertEquals(
+      equal(_exec(regExp, val), expected),
+      true,
+      `exec(${regExp}, ${val})() -> ${expected}`,
+    );
+  });
+});
