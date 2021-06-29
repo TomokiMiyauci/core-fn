0966977a3d1f369ecab482a9cabf22c71e9cc20f
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-06-29 20:54:27 +0900

    :sparkles:  Export non currying version

--- rollup.config.ts
+++ rollup.config.ts
@@ -6,6 +6,7 @@ import { dependencies, main, module } from "./package.json";
 
 const baseDir = resolve(__dirname);
 const inputFilePath = resolve(baseDir, "mod.ts");
+const nonCurryInputFilePath = resolve(baseDir, "non_curry", "mod.ts");
 const banner =
   "/*! Copyright (c) 2021-present the Core-fn authors. All rights reserved. MIT license. */";
 const external = Object.keys(dependencies);
@@ -54,6 +55,49 @@ const config = [
 
     output: {
       file: module,
+      format: "es",
+      sourcemap: true,
+      banner,
+    },
+  },
+
+  {
+    input: nonCurryInputFilePath,
+    plugins: [
+      replace(replaceOption),
+      ts({
+        transpiler: "babel",
+        tsconfig: (resolvedConfig) => ({
+          ...resolvedConfig,
+          declaration: false,
+        }),
+      }),
+      terser(),
+    ],
+
+    external,
+
+    output: {
+      file: "dist/non-curry/index.cjs.js",
+      format: "cjs",
+      sourcemap: true,
+      banner,
+    },
+  },
+  {
+    input: nonCurryInputFilePath,
+    plugins: [
+      replace(replaceOption),
+      ts({
+        transpiler: "babel",
+      }),
+      terser(),
+    ],
+
+    external,
+
+    output: {
+      file: "dist/non-curry/index.es.js",
       format: "es",
       sourcemap: true,
       banner,
